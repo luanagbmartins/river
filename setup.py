a048a21328f407283c993047d0fd4639d5462ea5
luanagbmartins <luanagbmartins@gmail.com>
2022-12-06 16:16:12 -0300

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="river",
+    version="1.0",
+    description="River dummy package",
+    author="Luana G B Martins",
+    author_email="luanagbmartins@gmail.com",
+    packages=["river"],  # same as name
+)
